2d24fc52d8915d44f09431e0a706390c8d7c5539
dmaushov <dinislam_maushov@epam.com>
2020-03-18 12:45:25 +0300

    Updates

---
+++ precache-manifest.92e738848cf0d5da7e2ca0bba8ae99bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6d943c6dfe407853805eb557e781e64",
+    "url": "/svoyak/index.html"
+  },
+  {
+    "revision": "eb58fc83dc189e468311",
+    "url": "/svoyak/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "10cc7b57a12f73290530",
+    "url": "/svoyak/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "eb58fc83dc189e468311",
+    "url": "/svoyak/static/js/2.7627d94b.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/svoyak/static/js/2.7627d94b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10cc7b57a12f73290530",
+    "url": "/svoyak/static/js/main.3163f1c9.chunk.js"
+  },
+  {
+    "revision": "7d7059ba6c7a8913b7b0",
+    "url": "/svoyak/static/js/runtime-main.7d3ccfe2.js"
+  }
+]);
